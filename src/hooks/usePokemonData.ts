91391a6ea2c79c30f65515e3fedd08955ad6a0e6
Mariusz <casmar90@gmail.com>
2024-12-03 22:59:50 +0100

    add localStorage to minimalise network calls

--- src/hooks/usePokemonData.ts
+++ src/hooks/usePokemonData.ts
@@ -23,34 +23,52 @@ export function usePokemonData(shuffledPokemonList: string[]) {
       setPokemonData(filteredPokemonData);
 
       const staticImagesMap: { [key: string]: string } = {};
-      for (const pokemon of filteredPokemonData) {
-        try {
-          const staticImage = await extractFirstFrame(
-            pokemon.sprites.versions["generation-v"]["black-white"].animated
-              .front_default,
-          );
-          staticImagesMap[pokemon.name] = staticImage;
-        } catch (error) {
-          console.error(
-            `Failed to extract static image for ${pokemon.name}`,
-            error,
-          );
-        }
-      }
-      setStaticImages(staticImagesMap);
-
       const gifImagesMap: { [key: string]: string } = {};
-      for (const pokemon of filteredPokemonData) {
-        try {
-          const gifImage =
-            pokemon.sprites.versions["generation-v"]["black-white"].animated
-              .front_default;
 
-          gifImagesMap[pokemon.name] = gifImage;
-        } catch (error) {
-          console.error(`Failed to grab gif image for ${pokemon.name}`, error);
+      for (const pokemon of filteredPokemonData) {
+        const cachedStaticImage = localStorage.getItem(
+          `${pokemon.name}-static`,
+        );
+        const cachedGifImage = localStorage.getItem(`${pokemon.name}-gif`);
+
+        if (!cachedStaticImage) {
+          try {
+            const staticImage = await extractFirstFrame(
+              pokemon.sprites.versions["generation-v"]["black-white"].animated
+                .front_default,
+            );
+            staticImagesMap[pokemon.name] = staticImage;
+            localStorage.setItem(`${pokemon.name}-static`, staticImage);
+          } catch (error) {
+            console.error(
+              `Failed to extract static image for ${pokemon.name}`,
+              error,
+            );
+          }
+        } else {
+          staticImagesMap[pokemon.name] = cachedStaticImage;
+        }
+
+        if (!cachedGifImage) {
+          try {
+            const gifImage =
+              pokemon.sprites.versions["generation-v"]["black-white"].animated
+                .front_default;
+
+            gifImagesMap[pokemon.name] = gifImage;
+            localStorage.setItem(`${pokemon.name}-gif`, gifImage);
+          } catch (error) {
+            console.error(
+              `Failed to grab gif image for ${pokemon.name}`,
+              error,
+            );
+          }
+        } else {
+          gifImagesMap[pokemon.name] = cachedGifImage;
         }
       }
+
+      setStaticImages(staticImagesMap);
       setGifImages(gifImagesMap);
     }
 
@@ -62,7 +80,9 @@ export function usePokemonData(shuffledPokemonList: string[]) {
     return shuffledPokemonList
       .map((name) => pokemonData.find((p) => p.name === name))
       .filter((pokemon): pokemon is Pokemon => pokemon !== undefined);
-  }, [shuffledPokemonList, pokemonData]);
+  
+  
+      }, [shuffledPokemonList, pokemonData]);
 
   return {
     pokemonData: sortedPokemonData,
