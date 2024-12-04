22ca986c580fcb0f5317b31b61c0e137588a17b7
Mariusz <casmar90@gmail.com>
2024-12-04 13:03:05 +0100

    remove opacity

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -35,7 +35,7 @@ const Footer = () => {
           <img
             src="./apple-touch-icon.png"
             alt="GitHub Logo"
-            className="h-[32px] min-h-[32px] w-[32px] min-w-[32px] transition-transform duration-500 ease-in-out hover:rotate-1turn hover:scale-125 hover:transform hover:opacity-100"
+            className="h-[32px] min-h-[32px] w-[32px] min-w-[32px] transition-transform duration-500 ease-in-out hover:rotate-1turn hover:scale-125 hover:transform"
           />
         </a>
       </div>
