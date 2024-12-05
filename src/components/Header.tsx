3d60164e26e54b28313a1ae3d69a565d909c29e2
Mariusz <casmar90@gmail.com>
2024-12-05 10:38:43 +0100

    add text-nowrap

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Score } from "../utils/types";
 const Header: React.FC<Score> = ({ score, bestScore }) => (
   <header className="flex flex-wrap items-center justify-between gap-4 bg-gray-300 p-4 md:p-8">
     <h1 className="flex-1 text-2xl">PokeMemo</h1>
-    <div className="flex flex-1 justify-end text-end">
+    <div className="flex flex-1 justify-end text-nowrap text-end">
       score: {score}
       <br />
       best score: {bestScore}
